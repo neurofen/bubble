c72441ec6403d9c4be91b28b991370cc7105943f
James Pott <neurofen@gmail.com>
2012-05-04 11:17:16 +1000

    add progress indicators

---
+++ lib/bubble/process_monitor.rb
@@ -0,0 +1,9 @@
+class ProcessMonitor
+	def initialize max
+	  @progress = ProgressBar.new max, :bar, :counter, :percentage, :eta
+	end
+
+	def notify
+	  @progress.increment!
+	end
+end
---
+++ lib/bubble/spinner.rb
@@ -0,0 +1,29 @@
+require 'thread'
+
+Thread.abort_on_exception = true
+
+class Spinner
+  GLYPHS = ['|', '/', '-', "\\", '|', '/', '-', "\\"]
+
+  def initialize
+    @start = false
+  end
+
+  def start
+    @start = true
+    @t = Thread.new do
+      while @start
+        GLYPHS.each do |glyph|
+          print "\r#{glyph}"
+          sleep 0.15
+        end
+      end
+      print "\r"
+    end
+  end
+
+  def stop
+    @start = false
+    #@t.join
+  end
+end
